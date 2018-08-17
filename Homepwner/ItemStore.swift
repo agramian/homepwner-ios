5785f413263c1ed30845801b48c9fb5ebe703184
Abtin Gramian <abtin.gramian@gmail.com>
2018-08-17 10:05:24 -0700

    finish

--- Homepwner/ItemStore.swift
+++ Homepwner/ItemStore.swift
@@ -13,9 +13,6 @@ class ItemStore {
     var allItems = [Item]()
     
     init() {
-        for _ in 0..<5 {
-            createItem()
-        }
         let constantRow = Item(name: "No more items!", serialNumber: nil, valueInDollars: nil)
         allItems.append(constantRow)
     }
@@ -23,9 +20,30 @@ class ItemStore {
     @discardableResult func createItem() -> Item {
         let newItem = Item(random: true)
         
-        allItems.append(newItem)
+        allItems.insert(newItem, at: allItems.endIndex - 1)
         
         return newItem
+    }
+    
+    func removeItem(_ item: Item) {
+        if let index = allItems.index(of: item) {
+            allItems.remove(at: index)
+        }
+    }
+    
+    func moveItem(from fromIndex: Int, to toIndex: Int) {
+        if fromIndex == toIndex {
+            return
+        }
+        
+        // Get reference to object being moved so you can reinsert it
+        let movedItem = allItems[fromIndex]
+        
+        // Remove item from array
+        allItems.remove(at: fromIndex)
+        
+        // Insert item in array at new location
+        allItems.insert(movedItem, at: toIndex)
     }
     
 }
--- Homepwner/ItemsViewController.swift
+++ Homepwner/ItemsViewController.swift
@@ -27,8 +27,9 @@ class ItemsViewController: UITableViewController {
         
         cell.textLabel?.text = item.name
         cell.detailTextLabel?.text = item.valueInDollars != nil ? "$\(item.valueInDollars!)" : ""
-        if item.valueInDollars != nil {
+        if indexPath.row == (itemStore.allItems.endIndex - 1) {
             cell.textLabel?.font = UIFont.systemFont(ofSize: 20)
+            cell.detailTextLabel?.text = item.valueInDollars != nil ? "$\(item.valueInDollars!)" : ""
         }
         
         return cell
@@ -36,6 +37,50 @@ class ItemsViewController: UITableViewController {
     
     override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         return itemStore.allItems[indexPath.row].valueInDollars != nil ? 60 : 44
+    }
+    
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        // If the table view is asking to commit a delete command...
+        if editingStyle == .delete {
+            let item = itemStore.allItems[indexPath.row]
+            
+            let title = "Delete \(item.name)?"
+            let message = "Are you sure you want to delete this item?"
+            
+            let ac = UIAlertController(title: title, message: message, preferredStyle: .actionSheet)
+            
+            let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+            ac.addAction(cancelAction)
+            
+            let deleteAction = UIAlertAction(title: "Delete", style: .destructive, handler: { (action) -> Void in
+                // Remove the item from the store
+                self.itemStore.removeItem(item)
+                
+                // Also remove that row from the table view with an animation
+                self.tableView.deleteRows(at: [indexPath], with: .automatic)
+            })
+            ac.addAction(deleteAction)
+            
+            // Present the alert controller
+            present(ac, animated: true, completion: nil)
+        }
+    }
+    
+    override func tableView(_ tableView: UITableView, moveRowAt sourceIndexpath: IndexPath, to destinationIndexPath: IndexPath) {
+        // Update the model
+        itemStore.moveItem(from: sourceIndexpath.row, to: destinationIndexPath.row)
+    }
+    
+    override func tableView(_ tableView: UITableView, targetIndexPathForMoveFromRowAt sourceIndexPath: IndexPath, toProposedIndexPath proposedDestinationIndexPath: IndexPath) -> IndexPath {
+        // Prevent reordering to to last row
+        if proposedDestinationIndexPath.row == (itemStore.allItems.endIndex - 1) {
+            return sourceIndexPath
+        }
+        return proposedDestinationIndexPath
+    }
+    
+    override func tableView(_ tableView: UITableView, titleForDeleteConfirmationButtonForRowAt indexPath: IndexPath) -> String? {
+        return "Remove"
     }
     
     override func viewDidLoad() {
@@ -51,6 +96,40 @@ class ItemsViewController: UITableViewController {
         tableView.contentInset = insets
         tableView.scrollIndicatorInsets = insets
          */
+    }
+    
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        return indexPath.row != (itemStore.allItems.endIndex - 1)
+    }
+    
+    @IBAction func addNewItem(_ sender: UIButton) {
+        // Create a new item and add it to the store
+        let newItem = itemStore.createItem()
+        
+        // Figure out where that item is in the array
+        if let index = itemStore.allItems.index(of: newItem) {
+            let indexPath = IndexPath(row: index, section: 0)
+            
+            // Insert this new row into the table
+            tableView.insertRows(at: [indexPath], with: .automatic)
+        }
+    }
+    
+    @IBAction func toggleEditMode(_ sender: UIButton) {
+        // If you are currently in editing mode...
+        if isEditing {
+            // Change text of button to inform user of state
+            sender.setTitle("Edit", for: .normal)
+            
+            //Turn of editing mode
+            setEditing(false, animated: true)
+        } else {
+            // Change text of button to inform user of state
+            sender.setTitle("Done", for: .normal)
+            
+            // Enter editing mode
+            setEditing(true, animated: true)
+        }
     }
     
 }
